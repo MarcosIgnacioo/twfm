b711d4297bc6cd148ac76114b00c8c9e3cbb5839
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-09-24 14:55:43 -0700

    dasf

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="mx-auto border-orangi-400 border-4 p-4 rounded-xl m-4 self-center w-6/12 shadow-md">
+      <h1 className="font-bold text-2xl text-orangi-600">An Important Alert</h1>
+      <p className="text-md py-2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in metus ornare, volutpat mauris
+        sit amet, ornare est. Praesent massa nulla, lacinia sit amet neque sed, ornare porttitor ante.
+        Nunc vulputate ultrices sollicitudin. Quisque in tristique felis, vel blandit felis. Aliquam ut
+        vestibulum massa. Proin at lectus in risus ultrices interdum a fringilla nisi. Maecenas
+        tristique augue at pretium consectetur.
+      </p>
+      <div>
+        <button className="rounded mr-2 border-orangi-500 bg-orangi-300  border-4 px-2 py-1">Confirm</button>
+        <button className="rounded border-orangi-500 text-orangi-300  border-4 px-2 py-1">Cancel</button>
+      </div>
+    </div>
+
+
+  )
+}
+
+export default App
---
+++ src/notas.md
@@ -0,0 +1,12 @@
+# layers
+
+las layers son basicamennte espacios aislados donde los comonentes tienen sus estilos y pues si hay un componente con una clse o un id y fuera de esa layer ha y una regla que le establece un estilo, aunque el componente tenga ese id si la layer escribe tambien a  esa propiedad se tomara prioridad a la layer. Sirve para aislar de manera cool
+
+# shadows
+
+asi 
+```html
+      <div className="w-12 h-12 bg-blue-500 shadow shadow-red-500">wep</div>
+```
+
+se pueden escribir extensiones que tengan fuentes asi nomas le haces npm install y ya tienes las fuentes siempre
